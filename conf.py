9ec5389f2695fa59dcb2fa7cc86d5d38a1461e04
Dominik Schlösser <dsc@dosc.net>
2021-12-27 20:09:19 +0100

    Pub version 0.0.8

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ author = 'dsc'
 
 # The full version, including alpha/beta/rc tags
 # sync with setup.py
-release = '0.0.7'
+release = '0.0.8'
 
 
 # -- General configuration ---------------------------------------------------
