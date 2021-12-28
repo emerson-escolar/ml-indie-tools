146e6bb126158f0f0e15a87fc5ac9f59147febcd
Dominik Schlösser <dsc@dosc.net>
2021-12-28 08:36:17 +0100

    random sample 0.0.9

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ author = 'dsc'
 
 # The full version, including alpha/beta/rc tags
 # sync with setup.py
-release = '0.0.8'
+release = '0.0.9'
 
 
 # -- General configuration ---------------------------------------------------
