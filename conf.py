38c52ac9516f8bdffc14e062a531ef0c83c16872
Dominik Schlösser <dsc@dosc.net>
2021-12-28 10:20:50 +0100

    Tokenizer 0.0.10 tests

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ author = 'dsc'
 
 # The full version, including alpha/beta/rc tags
 # sync with setup.py
-release = '0.0.9'
+release = '0.0.10'
 
 
 # -- General configuration ---------------------------------------------------
