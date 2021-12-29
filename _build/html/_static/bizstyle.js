4c0b55ca51aed3789678872c02af3c31a756662e
Dominik Schlösser <dsc@dosc.net>
2021-12-29 14:02:44 +0100

    local mirror doc fixes

--- _build/html/_static/bizstyle.js
+++ _build/html/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("ml-indie-tools 0.0.13 documentation");
+        $("li.nav-item-0 a").text("ml-indie-tools 0.0.14 documentation");
     }
 });
