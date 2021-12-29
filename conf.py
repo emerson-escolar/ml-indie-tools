6e455148eceea849a03293d3f3f00ed0cee42267
Dominik Schlösser <dsc@dosc.net>
2021-12-29 17:15:31 +0100

    mess

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ author = 'dsc'
 
 # The full version, including alpha/beta/rc tags
 # sync with setup.py
-release = '0.0.14'
+release = '0.0.15'
 
 
 # -- General configuration ---------------------------------------------------
