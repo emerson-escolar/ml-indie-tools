eb2dabdafd7abf3cf00242ff169f1893f7380a53
Dominik Schlösser <dsc@dosc.net>
2021-12-29 13:54:01 +0100

    0.0.13 local mirror

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ author = 'dsc'
 
 # The full version, including alpha/beta/rc tags
 # sync with setup.py
-release = '0.0.11'
+release = '0.0.13'
 
 
 # -- General configuration ---------------------------------------------------
