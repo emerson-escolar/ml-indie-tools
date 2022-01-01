ba4f6e965b059d1b6cf0b98ee4095aebfc00881b
Dominik Schlösser <dsc@dosc.net>
2022-01-01 16:20:16 +0100

    bad api

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ author = 'dsc'
 
 # The full version, including alpha/beta/rc tags
 # sync with setup.py
-release = '0.0.19'
+release = '0.0.21'
 
 
 # -- General configuration ---------------------------------------------------
