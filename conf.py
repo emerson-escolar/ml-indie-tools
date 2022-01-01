b8f277a26fd2614b7e0384eacfb9c48bec309632
Dominik Schlösser <dsc@dosc.net>
2022-01-01 09:17:54 +0100

    0.0.17

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ author = 'dsc'
 
 # The full version, including alpha/beta/rc tags
 # sync with setup.py
-release = '0.0.16'
+release = '0.0.17'
 
 
 # -- General configuration ---------------------------------------------------
