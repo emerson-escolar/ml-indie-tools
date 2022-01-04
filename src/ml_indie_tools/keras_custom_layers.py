8460795b5bd20586823681f710471c4e0a6b477e
Dominik Schlösser <dsc@dosc.net>
2022-01-04 10:37:55 +0100

    nothing

--- src/ml_indie_tools/keras_custom_layers.py
+++ src/ml_indie_tools/keras_custom_layers.py
@@ -9,7 +9,7 @@ class ResidualBlock(layers.Layer):
     The residual block consists of two fully connected layers with units neurons 
     followed by two BatchNorms and ReLUs:
 
-    .. code-block:: none
+        .. code-block:: console:
 
         #   ┌──────────────────────────────────────────────────┐
         #   │  ┌─────┐  ┌──┐  ┌────┐    ┌─────┐  ┌──┐  ┌────┐  ▼
