d91df13c30fc60bb0c899042f6d2ea9f550e2ed5
Dominik Schlösser <dsc@dosc.net>
2022-01-04 12:56:51 +0100

    More doc diagrams

--- _build/html/_static/bizstyle.js
+++ _build/html/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("ml-indie-tools 0.0.27 documentation");
+        $("li.nav-item-0 a").text("ml-indie-tools 0.0.28 documentation");
     }
 });
