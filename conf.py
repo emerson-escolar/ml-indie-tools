24cda8534e42ebd836723cb8c50db32db83fde7a
Dominik Schlösser <dsc@dosc.net>
2022-01-06 17:01:27 +0100

    TPU: no eager

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ author = 'dsc'
 
 # The full version, including alpha/beta/rc tags
 # sync with setup.py
-release = '0.0.30'
+release = '0.0.31'
 
 
 # -- General configuration ---------------------------------------------------
