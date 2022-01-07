9f6ea076636cbb19edaa56b8efbc74e0e420b924
Dominik Schloesser <dsc@dosc.net>
2022-01-07 17:40:25 +0100

    ongoing

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ author = 'dsc'
 
 # The full version, including alpha/beta/rc tags
 # sync with setup.py
-release = '0.0.32'
+release = '0.0.33'
 
 
 # -- General configuration ---------------------------------------------------
