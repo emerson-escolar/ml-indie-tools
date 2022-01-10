7f907986727937156d381c78e0b610d4e7aec5ef
Dominik Schlösser <dsc@dosc.net>
2022-01-10 10:07:49 +0100

    strange vers

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ author = 'dsc'
 
 # The full version, including alpha/beta/rc tags
 # sync with setup.py
-release = '0.0.39'
+release = '0.0.40'
 
 
 # -- General configuration ---------------------------------------------------
