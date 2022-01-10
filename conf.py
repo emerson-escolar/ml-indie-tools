b0dcb79592a4bdab8b824cf707f3b1fc5955ce52
Dominik Schlösser <dsc@dosc.net>
2022-01-10 08:36:17 +0100

    wrong

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ author = 'dsc'
 
 # The full version, including alpha/beta/rc tags
 # sync with setup.py
-release = '0.0.37'
+release = '0.0.38'
 
 
 # -- General configuration ---------------------------------------------------
