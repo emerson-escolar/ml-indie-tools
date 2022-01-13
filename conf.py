82d30593de3a8c95ef23b0e4491cd0ccd8ff5526
Dominik Schlösser <dsc@dosc.net>
2022-01-13 09:47:10 +0100

    test join-by-add for multi-heads (49)

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ author = 'dsc'
 
 # The full version, including alpha/beta/rc tags
 # sync with setup.py
-release = '0.0.48'
+release = '0.0.49'
 
 
 # -- General configuration ---------------------------------------------------
