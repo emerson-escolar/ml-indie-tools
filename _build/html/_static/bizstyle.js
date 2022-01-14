273b8e3a03e817c7ced2755772d4a7307a96e2d8
Dominik Schlösser <dsc@dosc.net>
2022-01-14 11:11:04 +0100

    58

--- _build/html/_static/bizstyle.js
+++ _build/html/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("ml-indie-tools 0.0.57 documentation");
+        $("li.nav-item-0 a").text("ml-indie-tools 0.0.58 documentation");
     }
 });
