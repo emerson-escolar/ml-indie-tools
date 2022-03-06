24fc1fef78a162d3f576ff1fb57dd738adb1eba7
Dominik Schlösser <dsc@dosc.net>
2022-03-06 11:11:42 +0100

    Updated gutenberg url

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ author = 'dsc'
 
 # The full version, including alpha/beta/rc tags
 # sync with setup.py
-release = '0.0.58'
+release = '0.0.59'
 
 
 # -- General configuration ---------------------------------------------------
